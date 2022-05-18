78d9974091f6fc3a1c8edac052b452f9e826a4b1
김동영 <kimdong-yeong@gimdong-yeongs-MacBook-Pro.local>
2022-05-18 21:48:08 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import xx from "./xx.png";
 function App() {
+  const [tags, setTags] = useState([]);
+  const [name, setName] = useState("");
+
+  const onChangeTestName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onKeyKey = (e) => {
+    console.log(e.key);
+    console.log(e.keyCode);
+    if (e.key === "Enter") {
+      // setName(e.target.value);
+      setName("");
+      setTags([...tags, e.target.value]);
+    }
+  };
+
+  const deleteClick = (index) => {
+    console.log(index);
+    console.log("click delete");
+    setTags([...tags.slice(0, index), ...tags.slice(index + 1)]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="tag-container">
+        {/* <div className="tag">
+          <span>JavaScript</span>
+          <img src={xx} className="material-icons" />
+        </div>
+        <div className="tag">
+          <span>JavaScript</span>
+          <img src={xx} className="material-icons" />
+        </div> */}
+        {tags.map((tag, index) => {
+          return (
+            <div key={index} className="tag">
+              <span>{tag}</span>
+              <img
+                onClick={() => deleteClick(index)}
+                src={xx}
+                className="material-icons"
+              />
+            </div>
+          );
+        })}
+        <input
+          value={name}
+          onChange={onChangeTestName}
+          onKeyPress={onKeyKey}
+        ></input>
+      </div>
     </div>
   );
 }
